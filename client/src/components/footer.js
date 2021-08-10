a28551324c9b454cd35c84a58b5891b9119d9df8
XSylvainX <37834208+XSylvainX@users.noreply.github.com>
2021-08-10 21:56:19 +0200

    Add files via upload
    commit footer.js

---
+++ client/src/components/footer.js
@@ -0,0 +1,18 @@
+import "../style/footer.css"
+
+
+function Footer() {
+    return (
+        <div className="foot-ct">
+
+            <div className="row">
+                <div className="col justify-content center">
+                    <p>© Sylvain Carlier make with ♥</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
