4472b7b229f6f9f95ef6a33d1987cd0d15554efc
XSylvainX <37834208+XSylvainX@users.noreply.github.com>
2021-08-10 21:48:48 +0200

    Add files via upload
    update src content

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Navigation from './nav/navigation';
+import Footer from './components/footer';
+
+
+
+function App() {
+  return (
+
+    // <div className="Contain">
+    <div>
+      <Navigation />
+      <Footer />
+    </div>
+
+
+  );
+}
+
+
+
+export default App;
